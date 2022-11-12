0835cd1a006a0fed666dc883d42683b1fc553d84
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-11-12 17:51:19 -0600

    worked on handleDeleteCart

--- controllers/cart/handleDeleteCart.js
+++ controllers/cart/handleDeleteCart.js
@@ -1,18 +1,36 @@
-const handleDeleteCart=(req,res,db)=>{
+const handleDeleteCart=async(req,res,db)=>{
   const {id}= req.params;
-  db('cart')
-    .where({id})
-    .del()
-    .then(resp=>{
-      if(resp){
-        res.json("cart successfully deleted")
-      }else{
-        res.status(400).json("Unable to delete cart")
-      }
+  const cartItemQuery= await db.select('*').from('cart_item').where({cart_id:id})
+  const trx = await db.transaction()
+  try{
+    const cartItemPromise= cartItemQuery.map(async(cart_item)=>{  
+      await trx('cart_item').where(cart_item).del()
+      return cart_item
     })
-    .catch(e=>{
-      res.status(400).json('error deleting cart');
-    })
+    const cartItemsArr=await Promise.all(cartItemPromise)
+    const cartQuery= await trx.select('*').from('cart').where({id})
+    const cart=(cartQuery.length>0)?cartQuery[0]:{}
+    await trx('cart').where({id}).del()
+    await trx.commit()
+    res.send({cart_id:id,cartItemsArr,cart})
+  }catch(err){
+    await trx.rollback(err)
+    res.status(400).json(`error deleting cart: ${err}`)
+  }
+
+  // db('cart')
+  //   .where({id})
+  //   .del()
+  //   .then(resp=>{
+  //     if(resp){
+  //       res.json("cart successfully deleted")
+  //     }else{
+  //       res.status(400).json("Unable to delete cart")
+  //     }
+  //   })
+  //   .catch(e=>{
+  //     res.status(400).json('error deleting cart');
+  //   })
 }
 
 module.exports={handleDeleteCart};
@@ -20,3 +38,25 @@ module.exports={handleDeleteCart};
 
 //need to account for cascading effects
 //delete cart_items
+
+
+
+
+// const {order_id}= req.params;
+// const orderItemQuery= await db.select('*').from('order_item').where({order_id})
+// const trx = await db.transaction()
+// try{
+//   const orderItemPromise= orderItemQuery.map(async(order_item)=>{  
+//     await trx('order_item').where(order_item).del()
+//     return order_item
+//   })
+//   const orderItemsArr=await Promise.all(orderItemPromise)
+//   const orderQuery= await trx.select('*').from('orders').where({order_id})
+//   const order=(orderQuery.length>0)?orderQuery[0]:{}
+//   await trx('orders').where({order_id}).del()
+//   await trx.commit();
+//   res.send({order_id,orderItemsArr,order})
+// }catch(err){
+//   await trx.rollback(err)
+//   res.status(400).json(`error deleting order: ${err}`)
+// }
--- controllers/orders/handleGetOrders.js
+++ controllers/orders/handleGetOrders.js
@@ -2,13 +2,11 @@ const handleGetOrders= async(req,res,db)=>{
   try{
     const orderIdPromise= await db.select('*').from('orders')
     if(orderIdPromise.length>0){
-
       const promiseArr=orderIdPromise.map(async(obj)=>{
         let order_id=obj.order_id
         let orderItems= await db.select('*').from('order_item').where({order_id})
         let productIds=orderItems.map(obj=>obj.product_id)
         let productsArr= await db.select('*').from('products').whereIn('product_id',productIds)
-
         return({
           order:obj,
           products:productsArr,
@@ -16,7 +14,6 @@ const handleGetOrders= async(req,res,db)=>{
         })
       })
       Promise.all(promiseArr).then((results)=> {
-        console.log(results)
         res.json(results)
       })
       
@@ -27,5 +24,4 @@ const handleGetOrders= async(req,res,db)=>{
     res.status(400).json(`error accessing orders: ${err}`)
   }
 }
-
 module.exports={handleGetOrders};
