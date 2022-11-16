ca5f5857b6b946adfac1e6013460fe9ead8fa1f7
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-11-15 20:09:22 -0600

    worked on handleGetCart and handleGetUser

--- controllers/cart/handleGetCart.js
+++ controllers/cart/handleGetCart.js
@@ -1,5 +1,13 @@
-const handleGetCart=(req,res,db)=>{
-  console.log("test")
+const handleGetCart=async(req,res,db)=>{
+  const {id}= req.params;
+  try{
+    const cartItemQuery= await db.select('*').from('cart_item').where({cart_id:id})
+    const cartQuery= await db.select('*').from('cart').where({id})
+    res.json({cart:cartQuery,cartItems:cartItemQuery})
+  }catch(err){
+    res.status(400).json(`error connecting to tables: ${err}`)
+  }
+
 }
 
 module.exports={handleGetCart};
--- controllers/users/handleGetUser.js
+++ controllers/users/handleGetUser.js
@@ -1,17 +1,15 @@
-const handleGetUser=(req,res,db)=>{
+const handleGetUser=async(req,res,db)=>{
   const {user_id}=req.params;
-  db.select('*').from('users').where({user_id})
-  .then(user=>{
-    if(user.length){
-      res.json(user[0]);
-    }else{
-      res.status(400).json('Not Found')
-    }
-  })
-  .catch(err=>res.status(400).json('error getting user'))
+  try{
+    const userQuery= await db.select('*').from('users').where({user_id})
+    const cartQuery= await db.select('*').from('cart').where({user_id})
+    user=userQuery.length>0?userQuery[0]:{}
+    cart=cartQuery.length>0?cartQuery[0]:{}
+    const cart_id=cart.hasOwnProperty('id')?cart.id:null;
+    const cartItems= await db.select('*').from('cart_item').where({cart_id})
+    res.json({user,cart,cartItems})
+  }catch(err){
+    res.status(400).json(`error connecting to tables: ${err}`)
+  }
 };
-
-
-
-
 module.exports={handleGetUser};
